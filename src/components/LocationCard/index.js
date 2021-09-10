c0d5969623cfff526a41600b3f2785c135ee3bed
Shav <shavhugan@gmail.com>
2021-09-10 01:46:52 +0100

    chanegd location card styling

--- src/components/LocationCard/index.js
+++ src/components/LocationCard/index.js
@@ -1,15 +1,23 @@
 import React, { useEffect, useRef } from "react";
 import "./style.css";
 import { Link, useParams } from "react-router-dom";
+import Icon from '@mdi/react'
+import { mdiHiking, mdiFoodForkDrink, mdiGlassCocktail } from '@mdi/js'
+
 
 const LocationCard = ({ name, id, iconClass, border }) => {
   return (
 
-    <div className='rounded-lg border-2 px-5 my-5' style={{ borderColor: `${border}` }}>
-      <div className='flex flex-row justify-between mt-3 mb-2'>
-        <Link to={`/pins/${id}`}><h1>{name}</h1></Link>
-        <p className='hidden'>{id}</p>
-        <i className={iconClass}></i>
+    <div className='rounded-3xl border p-5 my-5 h-auto cardhover' style={{ borderColor: `${border}` }}>
+      <div className='flex flex-col justify-between'>
+        <Link to={`/pins/${id}`}><h1 className="text-xl mb-4">{name}</h1></Link>
+        <p className='hidden'>{id}{iconClass}</p>
+        <Icon path={iconClass}
+          title="Profile"
+          size={2}
+          className="mx-auto"
+          color={border}
+        />
       </div>
     </div>
 
