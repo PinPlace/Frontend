74fbce56fc074b53986a7cf5fea0d0c6897112f2
Shav <shavhugan@gmail.com>
2021-09-10 02:28:44 +0100

    list card styling

--- src/components/ListCard/index.js
+++ src/components/ListCard/index.js
@@ -1,19 +1,23 @@
 import React, { useEffect, useRef } from "react";
 import "./style.css";
 import { NavLink, useParams } from "react-router-dom";
-import { mdiBorderColor } from "@mdi/js";
+import Icon from '@mdi/react'
 
 const ListCard = ({ name, id, iconClass, border }) => {
   return (
-    <NavLink to={`/lists/${id}`}>
-      <div className='rounded-lg border-2 px-5 my-5' style={{ borderColor: `${border}` }}>
-        <div className='flex flex-row justify-between mt-3 mb-2'>
-          <h1>{name}</h1>
-          <p className='hidden'>{id}</p>
-          <i className={iconClass}></i>
-        </div>
+    <div className='rounded-3xl border p-5 my-5 h-auto cardhover ' style={{ borderColor: `${border}` }}>
+      <div className='flex flex-col justify-between'>
+        <NavLink to={`/lists/${id}`}>
+          <h1 className="text-xl mb-4">{name}</h1></NavLink>
+        <p className='hidden'>{id}</p>
+        <Icon path={iconClass}
+          title="Profile"
+          size={2}
+          className="mx-auto"
+          color={border}
+        />
       </div>
-    </NavLink>
+    </div>
   );
 };
 
