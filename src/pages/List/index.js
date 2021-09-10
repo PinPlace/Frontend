a8149d6826f14cf9ff12b25dd87466d2dcc01e52
Shav <shavhugan@gmail.com>
2021-09-10 10:18:55 +0100

    added button to list page

--- src/pages/List/index.js
+++ src/pages/List/index.js
@@ -4,6 +4,10 @@ import { withRouter } from "react-router-dom";
 import { LocationCard, AddButton } from "../../components";
 import { useSelector, connect } from "react-redux";
 import { ListIndex } from "..";
+import Icon from '@mdi/react'
+import { mdiMapMarkerPlus } from '@mdi/js'
+import { NavLink } from "react-router-dom";
+
 
 const List = (props) => {
   let name = props.match.params.id;
@@ -42,7 +46,19 @@ const List = (props) => {
           <div className="flex flex-row justify-between"><h3 className="text-4xl font-semibold leading-normal text-center">
             {listTitle}
           </h3>
+            <NavLink to="/map" activeClassName="current" >
+              <button
+                className="gradscheme transform transition-all hover:scale-110 active:bg-lightBlue-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded-md outline-none focus:outline-none sm:mr-2 mb-1 md:pt-3 md:pr-5 ease-linear transition-all duration-150"
+                type="button"
 
+              >
+                <Icon path={mdiMapMarkerPlus}
+                  title="Add Pin"
+                  size={1}
+                  className="inline md:mb-1 md:mr-1"
+                /> <span className="hidden md:inline-block"> Add Pins </span>
+              </button>
+            </NavLink>
 
 
           </div>
